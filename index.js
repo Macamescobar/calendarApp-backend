fba91ce00ba020d0e17e9520d62382a598aa29b4
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-03-20 18:04:17 -0300

    send response

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ const express = require('express');
 require('dotenv').config();
 
 // Crear el servidor de express
-
 const app = express();
-
 
 // Directorio publico 
 app.use( express.static('public'));
 
+//Lectura y parseo del body
+app.use(express.json());
+
 
 //Rutas
-/* app.get('/', (req, resp) => {
-    
-    resp.json({
-        ok: true
-    })
-}); */
+app.use( '/api/auth', require('./routes/auth'));
 
+// TODO: CRUD: Eventos
 
 //Escuchar peticiones
-
 app.listen( process.env.PORT, () => {
     console.log(`servidor corriendo en puerto ${ process.env.PORT }`);
 })
---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { Router }= require('express');
+const router = Router();
+
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
+
+router.post('/new', createUser );
+
+router.post('/', loginUser );
+
+router.get('/renew', renewToken);
+
+module.exports = router;
