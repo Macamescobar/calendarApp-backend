d839dd731bf57e926258d2eeec279dc292e84dc9
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-03-20 22:34:07 -0300

    add middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,20 @@
-const {response} = require('express');
+const { response } = require('express');
+const { validationResult} = require('express-validator');
 
 const createUser = (req, res = response) => {
 
    const { name, email, password } = req.body;
 
-    res.json({
+    // manejo de errores
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -16,6 +26,14 @@ const createUser = (req, res = response) => {
 const loginUser = (req, res = response) => {
 
     const { email, password } = req.body;
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
 
     res.json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,31 @@
-const express = require('express');
+const express = require("express");
 
-const { Router }= require('express');
+const { Router } = require("express");
+const { check } = require("express-validator");
 const router = Router();
 
-const { createUser, loginUser, renewToken } = require('../controllers/auth');
+const { createUser, loginUser, renewToken } = require("../controllers/auth");
 
-router.post('/new', createUser );
+router.post(
+  "/new",
+  [
+    // middlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({min: 6}),
+  ],
+  createUser
+);
 
-router.post('/', loginUser );
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({ min: 6}),
+  ],
+  loginUser
+);
 
-router.get('/renew', renewToken);
+router.get("/renew", renewToken);
 
 module.exports = router;
