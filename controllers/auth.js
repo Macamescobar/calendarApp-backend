c2aab558961490de01e5e42871fdd1459314a8b3
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-03-30 18:18:57 -0300

    generate JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,36 @@
 const { response } = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 const createUser = async (req, res = response) => {
 
-   //const { name, email, password } = req.body;
+   const { email, password } = req.body;
 
    try {
-        const user = new User( req.body);
+        
+        let user = await User.findOne({ email });
+
+        if ( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User already exists'
+            });
+        }
+
+        user = new User( req.body);
+
+        //Encriptar password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+        
         await user.save();
- 
+
         res.status(201).json({
             ok: true,
-            msg: 'registro',
+            uid: user.id,
+            name: user.name
         });
+        
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -23,16 +41,49 @@ const createUser = async (req, res = response) => {
   
 }
 
-const loginUser = (req, res = response) => {
-    
+const loginUser = async(req, res = response) => {
+
     const { email, password } = req.body;
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-        
-    });
+
+    try {
+
+        const user = await User.findOne({ email });
+
+        if ( !user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: "Login failed! Email or Password wrong."
+            });
+        }
+
+        // Confirmar los passwords
+
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if( !validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: ' Password wrong '
+            });
+        }
+
+        //Generar JWT
+
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Please speak with the administrator'
+        })
+    }
+
+    
 }
 
 const renewToken = (req, res = response) => {
